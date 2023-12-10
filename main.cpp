31675490cc01aa2604de25fed4a25bccbfafcd66
hosein12a <153413720+hosein12a@users.noreply.github.com>
2023-12-09 21:51:29 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main () {
+char word;
+cout<<"please enter an enghlish word:";
+cin>>word;
+ 
+switch (word) {
+	case 'A':
+		cout<<"vowel";
+		break;
+	case 'a':
+		cout<<"vowel";
+		break;
+	case 'E':
+		cout<<"vowel";
+		break;
+	case 'e':
+		cout<<"vowel";
+		break;
+	case 'I':
+		cout<<"vowel";
+		break;
+	case 'i':	
+		cout<<"vowel";
+		break;
+	case 'U':
+		cout<<"vowel";
+		break;
+	case 'u':
+		cout<<"vowel";
+		break;
+	case 'O':
+	    cout<<"vowel";
+		break;
+	case 'o':
+	    cout<<"vowel";
+		default:
+		cout<<"not vowel";			
+}	
+}
